2f135158f457052477f8ab0d1d9659bc3e9f24f0
swiftins <natalja.rejenta@gmail.com>
2024-12-01 10:48:31 +0200

    Home work OP04

--- 1.py
+++ 1.py
@@ -1,4 +1,10 @@
+# Напишите функцию sum_range(start, end), которая суммирует все целые числа от значения 
+# start до величины end включительно.
+
+
 def sum_range(start, end):
-  return sum(range(start, end + 1))
+    return sum(range(start, end + 1))
 result = sum_range(10, 20)
-print(result)
+print('ChatGPT result = ', result)
+
+
